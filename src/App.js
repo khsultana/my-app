f44bac79386c709b8305e4cdbad5106d8aa87d29
khsultana <khsultana1995@gmail.com>
2022-09-20 22:38:13 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+const myStyle = { backgroundColor: 'blue ' }
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Person name="Natok korro"></Person>
+      <Person></Person>
+      <Person></Person>
+      <Friend></Friend>
+    </div >
   );
 }
 
+
+function Person(props) {
+  console.log(props)
+  // return 
+  return (<div className='person'>
+    <h1>BanglaDesh </h1>
+    <p>Its our country</p>
+  </div>
+  )
+}
+
+function Friend() {
+  return (
+    <div className='Container'>
+      <h3>Name:Kh SUltaba</h3>
+      <p style={myStyle}>Job:Saradin Khate</p>
+    </div>
+  )
+}
 export default App;
+
